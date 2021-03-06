819e6e545e782a52e249a93d8164b6e150ecb5c3
Vlad Sydorchuk <73775955+htmlcsscourse@users.noreply.github.com>
2021-03-06 14:48:09 +0200

    update json, style, html

--- js/script.js
+++ js/script.js
@@ -1,5 +1,3 @@
-
-
 getDataFromJSON();
 
 function getDataFromJSON() {
@@ -13,6 +11,7 @@ function getDataFromJSON() {
         })
         .then(json => {
             generateMonthes(json, 2);
+            generateLegend(json.teammates);
         })
         .catch(err => {
             console.error(err);
@@ -22,41 +21,85 @@ function getDataFromJSON() {
 }
 
 function generateMonthes(data, monthCount) {
-    console.log(data);
-    // let date = new Date(2021, 3, 3);
-    let date = new Date()
-    let grid = document.querySelector('.calendar__grid');
+    const container = document.querySelector('.calendar__grid');
 
+    // console.log(data);
+    let today = new Date(2021, 2, 7);
+    // let today = new Date();
+    let firstDay = new Date()
     for(let i = 0; i < monthCount; i++) {
-        var firstDay = new Date(date.getFullYear(), date.getMonth(), 1);
-        var lastDay = new Date(date.getFullYear(), date.getMonth() + 1, 0);
+        firstDay = new Date(firstDay.getFullYear(), firstDay.getMonth(), 1);
+        var lastDay = new Date(firstDay.getFullYear(), firstDay.getMonth() + 1, 0);
 
         firstDay.setDate(firstDay.getDate() - firstDay.getDay() + 1);
         
+        let monthTitle = ``
+        let daysTitle = ``;
         let dates = ``;
+        let count = 0;
         for (let currentDate = firstDay; currentDate <= lastDay; ) {
 
-            let isDayOff = isWeekend(currentDate, data.dayOffs);
+            if (count == 0) {
+                monthTitle = `<p class='calendar__item-title'>${getMonthName(lastDay)}</p>`;
+            }
+
+            if (++count <= 7) {
+                daysTitle += `<p class='calendar__item-day-title'>${getDayName(currentDate)}</p>`;
+            }
+
             dates += (currentDate.getMonth() != lastDay.getMonth()) 
             ? `<div class='calendar__item-day'></div>` 
-            : `<div class='calendar__item-day ${isDayOff ? 'weekend' : ''}'>${currentDate.getDate()}</div>`;
+            : `<div class='calendar__item-day 
+                ${isPastDay(today, currentDate) ? 'past-day' : ''} 
+                ${isWeekend(currentDate, data.dayOffs) ? 'weekend' : ''} 
+                ${isToday(today, currentDate) ? 'today' : ''}
+                '><span>${currentDate.getDate()}</span></div>`;
 
             currentDate.setDate(currentDate.getDate() + 1);
         }
 
-        grid.insertAdjacentHTML('beforeEnd', `<div class='calendar__item'>${dates}</div>`);
-        date = new Date(lastDay.getFullYear(), lastDay.getMonth() + 1, 1);
+        container.insertAdjacentHTML('beforeEnd', `<div class='calendar__item'>${monthTitle} ${daysTitle} ${dates}</div>`);
+        firstDay = new Date(lastDay.getFullYear(), lastDay.getMonth() + 1, 1);
     }
 }
 
-function isWeekend(date, dayOffs) {
-    let d = date.toISOString().slice(0,10);
-    console.log(d);
-    console.log(dayOffs);
+function generateLegend(teammates) {
+    const container = document.querySelector('.calendar__teammates');
+    container.insertAdjacentHTML('beforeEnd', teammates.map(item => (
+        `<div class="calendar__teammates-item">
+            <img class="calendar__teammates-img" src="${item.img}" alt="${item.name}">
+            <span class="calendar__teammates-name">${item.name}</span>
+            <div class="calendar__teammates-color" style="background-color: ${item["background-color"]};"></div>
+        </div>`
+    )).join(''));
 
-    return date.getDay() == 6 || date.getDay() == 0;// || dayOffs.indexOf(date.toISOString().slice(0,10));
+    // `<div class='calendar__item'>${monthTitle} ${daysTitle} ${dates}</div>`
+
+    console.log(teammates);
 }
 
+function isWeekend(date, dayOffs) {
+    let d = `${date.getFullYear()}-${(date.getMonth() + 1).toString().padStart(2, '0')}-${date.getDate().toString().padStart(2, '0')}`
+    return date.getDay() == 6 || date.getDay() == 0 || dayOffs.indexOf(d) != -1;
+}
+
+function isPastDay(today, date) {
+    return today > date;
+}
+
+function isToday(today, date) {
+    return today.toLocaleDateString() == date.toLocaleDateString();
+}
+
+function getMonthName(date) {
+    return  new Intl.DateTimeFormat('en-US', { month: "long" }).format(date);
+}
+
+
+
+function getDayName(date) {
+    return new Intl.DateTimeFormat('en-US', { weekday: "short" }).format(date);
+}
 
 
 
