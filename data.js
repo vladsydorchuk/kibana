83c202eaf8a9ab4ba2f1870ce228d92a77ab7050
vlad.sydorchuk <vladsydorchuk.ln@gmail.com>
2021-03-13 12:49:38 +0200

    upd

---
+++ data.js
@@ -0,0 +1,91 @@
+function getData() {
+    return {
+        "mates": [
+            {
+                "name": "Artem",
+                "img": "https://raw.githubusercontent.com/vladsydorchuk/FalconsKibana/master/images/teammates/Artem.jpg",
+                "backgroundColor": "#cef39e",
+                "dayOffs": [],
+                "id": "artem",
+                "startDate": "",
+                "endDate": ""
+            },
+            {
+                "name": "Ivan",
+                "img": "https://raw.githubusercontent.com/vladsydorchuk/FalconsKibana/master/images/teammates/Ivan.jpg",
+                "backgroundColor": "#9eabf3",
+                "dayOffs": [],
+                "id": "ivan",
+                "startDate": "",
+                "endDate": ""
+            },
+            {
+                "name": "Sergii",
+                "img": "https://raw.githubusercontent.com/vladsydorchuk/FalconsKibana/master/images/teammates/Sergii.jpg",
+                "backgroundColor": "#9ef3e8",
+                "dayOffs": [],
+                "id": "sergii",
+                "startDate": "",
+                "endDate": ""
+            },
+            {
+                "name": "Vlad",
+                "img": "https://raw.githubusercontent.com/vladsydorchuk/FalconsKibana/master/images/teammates/Vlad.jpg",
+                "backgroundColor": "#f3b69e",
+                "dayOffs": [],
+                "id": "vlad",
+                "startDate": "",
+                "endDate": "" 
+            },
+            {
+                "name": "Nastia",
+                "img": "https://raw.githubusercontent.com/vladsydorchuk/FalconsKibana/master/images/teammates/Nastia.jpg",
+                "backgroundColor": "#f39ede",
+                "dayOffs": [],
+                "id": "nastia",
+                "startDate": "",
+                "endDate": ""
+            },
+            {
+                "name": "No name",
+                "img": "https://raw.githubusercontent.com/vladsydorchuk/FalconsKibana/master/images/teammates/Default.jpg",
+                "backgroundColor": "#f3f39e",
+                "dayOffs": [],
+                "id": "noname",
+                "startDate": "2021-04-05T00:00:00",
+                "endDate": ""
+            }
+        ],
+        "currentSprint": {
+            "startDate": "",
+            "endDate": ""
+        },
+        "monthCount": 2,
+        "startFromMate": 4,
+        "startDate": "2021-03-01T00:00:00",
+        "startSprintDate": "2021-02-22T00:00:00",
+        "dayOffs": [
+            "2021-01-01T00:00:00",
+            "2021-01-07T00:00:00",
+            "2021-01-08T00:00:00",
+            "2021-03-08T00:00:00",
+            "2021-05-03T00:00:00",
+            "2021-05-04T00:00:00",
+            "2021-05-10T00:00:00",
+            "2021-06-21T00:00:00",
+            "2021-06-28T00:00:00",
+            "2021-09-23T00:00:00",
+            "2021-09-24T00:00:00",
+            "2021-10-14T00:00:00",
+            "2021-10-15T00:00:00",
+            "2021-12-27T00:00:00"
+        ],
+        "workDays": [
+            "2021-01-16T00:00:00",
+            "2021-09-28T00:00:00",
+            "2021-10-23T00:00:00"
+        ]
+    }
+}
+
+export { getData }
